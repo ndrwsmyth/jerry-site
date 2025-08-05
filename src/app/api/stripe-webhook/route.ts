6bb1e327ff749c725ef2bbe035267ae63616ce1c
ndrwsmyth <116239497+andrew1north@users.noreply.github.com>
2025-08-04 22:43:27 -0400

    debugged and validated webhook, ready for prod testing

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -4,7 +4,7 @@ import { writeClient } from "@/sanity/client";
 import { revalidatePath } from "next/cache";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-07-30.basil", 
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
